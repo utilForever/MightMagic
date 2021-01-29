8c4537fb4da05136ef43248e5b6e4e3f5925e45c
Chris Ohk <utilforever@gmail.com>
2021-01-29 23:19:24 +0900

    feat(faction): Add method 'new()'

--- src/models/faction.rs
+++ src/models/faction.rs
@@ -1,3 +1,10 @@
 pub struct Faction {
     pub creatures: (String, String)
 }
+
+impl Faction {
+    #[inline]
+    pub const fn new() -> Faction {
+        Faction { creatures: (String::new(), String::new()) }
+    }
+}
