7fa129c031f6eef95976ec629506eef8086af27d
Chris Ohk <utilforever@gmail.com>
2021-01-29 23:21:00 +0900

    build: Add module files for loader and model

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,2 @@
-pub fn add(x: i32, y: i32) -> i32 {
-    x + y
-}
-
-#[cfg(test)]
-mod test {
-    #[test]
-    fn test_add() {
-        use super::*;
-        assert_eq!(add(3, 4), 7);
-    }
-}
+pub mod loaders;
+pub mod models;
