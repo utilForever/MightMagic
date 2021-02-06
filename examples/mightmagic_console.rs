1d80ac92f24c3810621368f1065195391fd8df64
Chris Ohk <utilforever@gmail.com>
2021-02-06 20:15:38 +0900

    feat(faction): Add function 'print_splash()'

--- examples/mightmagic_console.rs
+++ examples/mightmagic_console.rs
@@ -1,7 +1,21 @@
 use mightmagic::loaders::faction_loader::FactionLoader;
 use mightmagic::models::faction::Faction;
 
+fn print_splash() {
+    println!("{}", "
+     __  __ _       _     _   __  __             _      
+    |  \\/  (_)     | |   | | |  \\/  |           (_)     
+    | \\  / |_  __ _| |__ | |_| \\  / | __ _  __ _ _  ___ 
+    | |\\/| | |/ _` | '_ \\| __| |\\/| |/ _` |/ _` | |/ __|
+    | |  | | | (_| | | | | |_| |  | | (_| | (_| | | (__ 
+    |_|  |_|_|\\__, |_| |_|\\__|_|  |_|\\__,_|\\__, |_|\\___|
+               __/ |                        __/ |       
+              |___/                        |___/        ");
+}
+
 fn main() {
+    print_splash();
+
     let mut factions: Vec<Faction> = Vec::new();
     FactionLoader::load(&mut factions);
 
