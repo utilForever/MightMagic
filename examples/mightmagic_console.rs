518d4dc0fecefbda54a11bc48159c3414fa44716
Chris Ohk <utilforever@gmail.com>
2021-02-06 23:54:26 +0900

    feat(faction): Apply changes of faction-related code

--- examples/mightmagic_console.rs
+++ examples/mightmagic_console.rs
@@ -1,8 +1,9 @@
-use mightmagic::loaders::faction_loader::FactionLoader;
-use mightmagic::models::faction::Faction;
+use mightmagic::managers::resource_manager::ResourceManager;
 
 fn print_splash() {
-    println!("{}", "
+    println!(
+        "{}",
+        "
      __  __ _       _     _   __  __             _      
     |  \\/  (_)     | |   | | |  \\/  |           (_)     
     | \\  / |_  __ _| |__ | |_| \\  / | __ _  __ _ _  ___ 
@@ -10,16 +11,19 @@ fn print_splash() {
     | |  | | | (_| | | | | |_| |  | | (_| | (_| | | (__ 
     |_|  |_|_|\\__, |_| |_|\\__|_|  |_|\\__,_|\\__, |_|\\___|
                __/ |                        __/ |       
-              |___/                        |___/        ");
+              |___/                        |___/        "
+    );
 }
 
 fn main() {
+    ResourceManager::initialize();
+
     print_splash();
 
-    let mut factions: Vec<Faction> = Vec::new();
-    FactionLoader::load(&mut factions);
-
-    for faction in factions {
-        println!("{}, {}", faction.creatures.0, faction.creatures.1);
+    let factions = ResourceManager::get_all_factions();
+    for faction in factions.iter() {
+        for creature in faction.creatures.iter() {
+            println!("{}, {}", creature.0, creature.1);
+        }
     }
 }
